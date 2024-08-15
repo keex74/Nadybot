7bf91ea401bdeaf4d2e7bca3d3d30bfde0907816
Nadyita <nadyita@hodorraid.org>
2024-08-15 20:47:02 +0200

    Remove unneeded models

--- src/Modules/EXPORT_MODULE/Schema/RaidBlockType.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Nadybot\Modules\EXPORT_MODULE\Schema;
-
-enum RaidBlockType: string {
-	case Points = 'points';
-	case Join = 'join';
-	case Bid = 'bid';
-}
