3d239019e1b17dee0aba1343e2f26632f69bfe18
Nadyita <nadyita@hodorraid.org>
2024-08-16 14:13:20 +0200

    Fix parse error of GSP songs

--- src/Modules/GSP_MODULE/Song.php
+++ src/Modules/GSP_MODULE/Song.php
@@ -5,7 +5,7 @@ namespace Nadybot\Modules\GSP_MODULE;
 class Song {
 	public function __construct(
 		public string $date,
-		public string $artwork,
+		public ?string $artwork=null,
 		public ?int $duration=null,
 		public string $artist='Unknown Artist',
 		public string $title='Unknown Song',
